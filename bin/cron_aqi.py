b991ec89c7d4444b1cb9e5f776e4026cc721093a
zhujiawei <aishenghuomeidaoli@163.com>
2018-01-04 14:35:20 +0800

    add crontab script

---
+++ bin/cron_aqi.py
@@ -0,0 +1,29 @@
+# _*_ coding: utf-8 _*_
+import os
+import json
+import logging
+import requests
+import sys
+import django
+
+PROJECT_DIR = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+sys.path.insert(0, PROJECT_DIR)
+
+env = 'settings_pro' if sys.argv[-1] == 'settings_pro' else 'settings'
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fatty.%s" % env)
+
+django.setup()
+
+logger = logging.getLogger('aqi')
+
+
+def main():
+    logger.info('-------------start crontab-------------')
+    resp = requests.get('http://127.0.0.1:8082/aqi/start/')
+    logger.info('crontab result: %s' % resp.content)
+    logger.info('-------------finish crontab-------------')
+    return
+
+
+if __name__ == '__main__':
+    main()
